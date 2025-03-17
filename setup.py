d1545f4b494bbed8a1d38afc26bf2defbab3ca30
ArefBz34 <160572598+ArefBz34@users.noreply.github.com>
2025-03-17 23:36:43 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="NeuroAI_proj",
+    name="NeuroAI",
     version="0.1.0",
     author="Mohammad Aref Bagherzadeh",
     author_email="aref.bagherzadeh.ab@gmail.com",
