e96456264a2375d96724df33554b9543e30e634e
ArefBz34 <160572598+ArefBz34@users.noreply.github.com>
2025-03-22 17:53:02 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="NeuroAI",
-    version="0.1.0",
+    version="0.1.7",
     author="Mohammad Aref Bagherzadeh",
     author_email="aref.bagherzadeh.ab@gmail.com",
     description="A new window to neuroradiology datasets",
